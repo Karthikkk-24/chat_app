e15b96755af36f97357335c55edaa2fb5b22a89c
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-02 09:57:58 +0530

    Completed Login Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ export default {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {
             colors: {
                 background: "#202022",
-                primary: "#dcdce6",
+                primary: "#dcdce6"
             }
         },
     },
