f9ce3dffe657fcccc34dd61d829e95e0d8629984
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-02 09:32:59 +0530

    Added routes for login and register, created Dashboard with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Dashboard from '../components/Dashboard';
+import Login from '../components/Login';
+import Register from '../components/Register';
 import './App.css';
 
 function App() {
@@ -6,7 +9,9 @@ function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" />
+				<Route path="/login" exact element={<Login />} />
+				<Route path="/register" exact element={<Register />} />
+				<Route path="/dashboard" exact element={<Dashboard />} />
 			</Routes>
 		</Router>
 	);
